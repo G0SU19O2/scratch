b76b23ad41c640282a899d67e2419d51fd3ce177
G0SU19O2 <huyquocnguyen1902@gmail.com>
2024-05-26 00:02:18 +0700

    setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/G0SU19O2/rssagg
+
+go 1.22.3
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	println(port)
+}
